2848b613e248ad06511e4499a4a20f4854d5f7d6
TOSHIBA2018 <TOSHIBA2018@192.168.1.7>
2019-06-03 18:10:46 -0400

    commit do Hilarison

---
+++ ProjetoPHP/Quemsomos.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="Index.css">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        
+        <!-- Parte do Vitor inicio -->
+        <header>
+            <a href="#logo" class='logo'>CyberSpace</a>
+            <div class="menu-toggle"></div>
+            <nav>
+                <ul>
+                <li><a href="index.html" class="active">Inicio</a></li>
+                <li><a href="#Oquefazemos?" >Oque fazemos?</a></li>
+                <li><a href="#QuemSomos:">Quem Somos?</a></li>
+                <li><a href="#Contato" >Contato</a></li>
+                <li><a href="Orçamentos:">Orçamento</a></li>
+                </ul>  
+               
+               
+                
+         </nav>
+             <div class="clearfix"></div>
+        </header>    
+<script src="http://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  $(document).ready(function(){
+      $('.menu-toggle').click(function(){
+          $('.menu-toggle').toggleClass('active');
+          $('nav').toggleClass('active');
+
+      });
+  });
+  </script>
+  
+  <header>
+   <div class="col-sm">
+      Coluna numero tres
+      <h2>Quem somos?</h2>
+    </div>
+</header>
+  
+ <div class="footer-main-div">
+              <div class="footer-social-icons">
+                  <ul>
+                      <li><a href="#" target="blank"><i class="fa fa-facebook"></i></a></li>
+                      <li><a href="#" target="blank"><i class="fa fa-youtube"></i></a></li>
+                      <li><a href="#" target="blank"><i class="fa fa-twitter"></i></a></li>
+                      <li><a href="#" target="blank"><i class="fa fa-google-plus"></i></a></li>
+
+                  </ul> 
+              </div>
+              <div class="footer-menu-one">
+                  <ul>
+                      <li><a href="#">Inicio</a></li>
+                      <li><a href="#">Produtos</a></li>
+                      <li><a href="#">Contato</a></li>                                         
+                      
+                      
+                  </ul>
+              </div>
+              
+              <div class="footer-menu-two">
+                  <ul>
+                      <li><a href="#">Quem somos?</a></li>
+                      <li><a href="#">Orçamento</a></li>                  
+                  </ul>
+              </div>
+          
+
+          </div>
+              </body>   
+      
+</html>
